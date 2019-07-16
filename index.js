b52362bc840e39d86fa05c1ea27c1cf5d182e131
Woof <gchen1258@gmail.com>
2019-07-16 01:34:04 -0400

    Fixes for nginx'

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const https = require('https')
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.post('/shopify', (req, res)=>{
   console.log(req.query)
