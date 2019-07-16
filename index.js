277c115cfcd39172c4dd7f8267d990633ddcf6ef
Woof <gchen1258@gmail.com>
2019-07-15 23:37:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const https = require('https')
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.post('/shopify', (req, res)=>{
+  console.log(req.query)
+  res.status(200)
+  res.send("OK")
+})
+
+app.get('/', (req, res)=>{
+  res.send('Hello')
+})
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app).listen(PORT, '0.0.0.0', ()=>{
+  console.log(`App listening on port ${PORT}`)
+})
